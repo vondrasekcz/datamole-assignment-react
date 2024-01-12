25b303df2895b4448f9176543e8998cb31b0232e
Pavel Vondrášek <vondrasekcz@gmail.com>
2024-01-12 18:07:29 +0100

    Create task

--- client/src/api/tasks.ts
+++ client/src/api/tasks.ts
@@ -5,8 +5,15 @@ export type Task = {
     title: string;
     done?: boolean;
 };
+export type CreateTaskInput = {
+    title: string;
+};
 
 const buildApiUrl = (path = ""): string => `${import.meta.env.VITE_HTTP_LINK}${path}`;
 
 export const getTasks = (): Promise<Task[]> =>
     axios.get(buildApiUrl("/items?_sort=createdAt&_order=desc")).then((res) => res.data);
+
+export function createTask(createTaskInput: CreateTaskInput): Promise<Task> {
+    return axios.post(buildApiUrl("/items"), createTaskInput).then((res) => res.data);
+}
