9e54c58c37e929c4c5bd3a28c0b8aeed21226357
Pavel Vondrášek <vondrasekcz@gmail.com>
2024-01-12 17:01:21 +0100

    Style ListItem

--- client/src/components/ListItem.tsx
+++ client/src/components/ListItem.tsx
@@ -10,8 +10,20 @@ const StyledDiv = styled.div`
     align-items: center;
 `;
 
+const InputContainer = styled.div`
+    flex-shrink: 0;
+`;
+
 const Label = styled.label`
     margin-left: 15px;
+    margin-right: 15px;
+    word-break: break-word;
+`;
+
+const Actions = styled.div`
+    display: flex;
+    margin-left: auto;
+    gap: 2px;
 `;
 
 export type LiteItemProp = CheckboxProps & {
@@ -22,13 +34,17 @@ export type LiteItemProp = CheckboxProps & {
 
 export const ListItem: React.FC<LiteItemProp> = ({ label, handleRemoval, handleEdit, ...checkboxProps }) => (
     <StyledDiv>
-        <Checkbox {...checkboxProps} />
+        <InputContainer>
+            <Checkbox {...checkboxProps} />
+        </InputContainer>
         <Label>{label}</Label>
-        <Button variant="primary" onClick={() => handleEdit()}>
-            <TrashIcon />
-        </Button>
-        <Button variant="error" onClick={() => handleRemoval()}>
-            <Pencil1Icon />
-        </Button>
+        <Actions>
+            <Button variant="primary" onClick={() => handleEdit()}>
+                <Pencil1Icon />
+            </Button>
+            <Button variant="error" onClick={() => handleRemoval()}>
+                <TrashIcon />
+            </Button>
+        </Actions>
     </StyledDiv>
 );
